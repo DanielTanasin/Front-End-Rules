db8638756447ae29b2be566ee85fe18134ab5827
Daniel Tanasin <daniel.tanasin@vega.local>
2022-11-17 16:48:13 +0100

    final changes

--- dist/js/accordion.js
+++ dist/js/accordion.js
@@ -1,47 +1,30 @@
+
+
 const questions = document.querySelectorAll('.question');
-let currentBox = null;
 
-questions.forEach((question)=>{
-    question.addEventListener('click', ()=>{
-        const parent = question.parentElement;
-        const panel = parent.querySelector('.answer');
+const openAcc = (question) =>{
+    const parent = question.parentElement;
+    const panel = parent.querySelector(".answer")
+    question.classList.add('question--active')
+    panel.style.maxHeight = panel.scrollHeight + "px"    
+}
+const closeAcc = (question) =>{
+    const parent = question.parentElement;
+    const panel = parent.querySelector(".answer")
+    question.classList.remove('question--active')
+    panel.style.maxHeight = null;
+}
 
-
-        questions.forEach(question => {
-            const parent = question.parentElement;
-            const panel = parent.querySelector(".answer");
-            question.classList.remove("question--active");
-            panel.style.maxHeight = null;
-        });
-
-        if(question === currentBox){   
-            questions.forEach(signleQuestion => {
-                const parent = signleQuestion.parentElement;
-                const panel = parent.querySelector(".answer");
-                setTimeout(()=>{
-                    panel.style.maxHeight = null;
-                    signleQuestion.classList.remove("question--active");
-                },1);
-            });
-        }
-        
-        if(!question.classList.contains('question--active')){
-            question.classList.add('question--active');
-        }else {
-            
-                question.classList.remove('question--active');
+questions.forEach((question) =>{
+    const parent = question.parentElement;
+    const panel = parent.querySelector(".answer")
     
-        }
-
+    question.addEventListener('click',() =>{
         if(panel.style.maxHeight){
-            panel.style.maxHeight = null;
-        }else {
-            panel.style.maxHeight = panel.scrollHeight + 'px';
+            closeAcc(question);
+        }else{
+            questions.forEach((question) => closeAcc(question));
+            openAcc(question);
         }
-
-        
-        currentBox = question;
     })
-    
-
 })
